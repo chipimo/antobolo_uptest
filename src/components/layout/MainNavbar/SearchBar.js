45822354e183899ac807443c034f9d41a554b38d
findit <53036391+finditDeveloper@users.noreply.github.com>
2019-10-29 04:21:18 +0200

    inital commit

--- src/components/layout/MainNavbar/SearchBar.js
+++ src/components/layout/MainNavbar/SearchBar.js
@@ -9,7 +9,8 @@ import Toolbar from "@material-ui/core/Toolbar";
 import IconButton from "@material-ui/core/IconButton";
 import Typography from "@material-ui/core/Typography";
 import CloseIcon from "@material-ui/icons/Close";
-import { Button } from "antd";
+import InputBase from "@material-ui/core/InputBase";
+import SearchIcon from "@material-ui/icons/Search";
 
 const useStyles = makeStyles(theme => ({
   appBar: {
@@ -91,12 +92,23 @@ function SearchBar(props) {
           >
             <CloseIcon />
           </IconButton>
-          <Typography variant="h6" className={classes.title}>
-            Sound
+
+          <Typography className={classes.title} variant="h6" noWrap>
+            Material-UI
           </Typography>
-          <Button autoFocus color="inherit" onClick={props.handleClose}>
-            save
-          </Button>
+          <div className={classes.search}>
+            <div className={classes.searchIcon}>
+              <SearchIcon />
+            </div>
+            <InputBase
+              placeholder="Search…"
+              classes={{
+                root: classes.inputRoot,
+                input: classes.inputInput
+              }}
+              inputProps={{ "aria-label": "search" }}
+            />
+          </div>
         </Toolbar>
       </AppBar>
       <List>
