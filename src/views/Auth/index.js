d0c2382d412fa7ada707ee7e5a0c0a7309f4df96
findit <53036391+finditDeveloper@users.noreply.github.com>
2019-10-30 08:55:23 +0200

    inital commt

--- src/views/Auth/index.js
+++ src/views/Auth/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Card, Col, Button } from "shards-react";
 import Login from "./Login";
+import { isMobile } from "react-device-detect";
 
 export default function UserAuth() {
   return (
@@ -19,47 +20,64 @@ export default function UserAuth() {
           small
           className="mb-4"
         >
-          <div style={{ width: "100%", display: "flex" }}>
-            <div
-              style={{
-                width: "40%",
-                height: 450,
-                display: "flex",
-                justifyContent: "center"
-              }}
-            >
-              <div style={{ marginTop: 90 }}>
-                <div style={{ width: "70%", margin: "auto" }}>
-                  <img
-                    id="main-logo"
-                    className="d-inline-block align-top mr-1"
-                    style={{ maxWidth: "200px" }}
-                    src={require("../../images/LOGO-whait.png")}
-                    alt="Antobolo"
-                  />
-                </div>
-                <div style={{ marginTop: 10, width: "60%", margin: "auto" }}>
-                  <h6 style={{ color: "#ccc" }}>
-                    Login usign social media to get quick access
-                  </h6>
-                </div>
-                <div
-                  style={{
-                    width: "65%",
-                    margin: "auto",
-                    marginTop: 50,
-                  }}
-                >
-                  <Button pill outline size="sm" className="mb-4">
-                    Login with Facebook
-                  </Button>
+          {isMobile ? (
+            <div style={{ width: "100%", display: "flex" }}>
+              <div
+                style={{
+                  width: "40%",
+                  height: 450,
+                  display: "flex",
+                  justifyContent: "center"
+                }}
+              >
+                <div>
+                  <Login />
                 </div>
               </div>
             </div>
-            <div style={{ backgroundColor: "#fff", width: "60%" }}>
-              <Login />
+          ) : (
+            <div style={{ width: "100%", display: "flex" }}>
+              <div
+                style={{
+                  width: "40%",
+                  height: 450,
+                  display: "flex",
+                  justifyContent: "center"
+                }}
+              >
+                <div style={{ marginTop: 90 }}>
+                  <div style={{ width: "70%", margin: "auto" }}>
+                    <img
+                      id="main-logo"
+                      className="d-inline-block align-top mr-1"
+                      style={{ maxWidth: "200px" }}
+                      src={require("../../images/LOGO-whait.png")}
+                      alt="Antobolo"
+                    />
+                  </div>
+                  <div style={{ marginTop: 10, width: "60%", margin: "auto" }}>
+                    <h6 style={{ color: "#ccc" }}>
+                      Login usign social media to get quick access
+                    </h6>
+                  </div>
+                  <div
+                    style={{
+                      width: "65%",
+                      margin: "auto",
+                      marginTop: 50
+                    }}
+                  >
+                    <Button pill outline size="sm" className="mb-4">
+                      Login with Facebook
+                    </Button>
+                  </div>
+                </div>
+              </div>
+              <div style={{ backgroundColor: "#fff", width: "60%" }}>
+                <Login />
+              </div>
             </div>
-          </div>
+          )}
         </Card>
       </Col>
     </div>
