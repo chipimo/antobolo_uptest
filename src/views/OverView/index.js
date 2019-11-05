07d402642a788c57d3d48897ea9fdbe123f9bd9d
findit <53036391+finditDeveloper@users.noreply.github.com>
2019-11-06 01:51:22 +0200

    inital commit

--- src/views/OverView/index.js
+++ src/views/OverView/index.js
@@ -1,11 +1,15 @@
-import React from 'react'
+import React from "react";
 
 export default function ItemOverView(props) {
-    return (
-        <div style={{paddingTop:20,paddingLeft:20}}>
-            <div>
-                
-            </div>
+  return (
+    <div style={{ paddingTop: 20, paddingLeft: 20 }}>
+      <div style={{ width: "100%", display: "flex" }}>
+        <div style={{width: "20%",}}>
+            
         </div>
-    )
+        <div style={{width: '45%',}}>data</div>
+        <div style={{width: '30%',}}>pric tag</div>
+      </div>
+    </div>
+  );
 }
