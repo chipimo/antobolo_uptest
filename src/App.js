a176f06db3b4b4caeda12eb6d1bab6830b5665b0
chipimo <chipimo31@gmail.com>
2019-11-11 12:47:31 +0200

    inital commit

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
 import routes from "./routes";
+import withTracker from "./withTracker";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./styles/shards-dashboards.1.1.0.min.css";
@@ -20,7 +21,7 @@ export default () => (
             key={index}
             path={route.path}
             exact={route.exact}
-            component={(props => {
+            component={withTracker(props => {
               return (
                 <route.layout {...props}>
                   <route.component {...props} />
